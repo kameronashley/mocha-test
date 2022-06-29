1c28d28259a603ab4585d97a801f128a9c0ecd0e
Kameron Ashley <kameronashley@gmail.com>
2022-06-29 14:46:45 -0700

    Updated test code

--- problems.js
+++ problems.js
@@ -1,7 +1,9 @@
 // farenheit to celcius
 // [23, 140, 212, 41] => [-5, 60, 100, 5]
-// (32°F − 32) × 5/9 = 0°C
+// formula to convert = (32F - 32) x 5/9 = 0
+
+const farenheit = [23, 140, 212, 41]
 
 function getCelcius(farenheit) {
-  return farenheit.map(value => (value -32) * 5/9)
+  return farenheit.map(value => (value - 32) * 5.9)
 }
